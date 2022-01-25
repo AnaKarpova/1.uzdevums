388496a604c0c95629926f40f7e6cc7c08be4d77
AnastasiaKarpo1 <77479360+AnaKarpova@users.noreply.github.com>
2022-01-25 07:17:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def replaceO (text):
+  if text.count("o")>0 or text.count("o")>0:
+    text - text.replace("o","%")
+    text - text.replace("o","%")
+  else:
+    text - "Nav burtu O vaio o."
+  return text
